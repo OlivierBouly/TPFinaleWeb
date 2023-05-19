6d57bf5fcbeb07056e3db9050ce6deecebf656cd
Olivier Bouly <olivierbouly13@gmail.com>
2023-05-19 12:29:19 -0400

    Updates

---
+++ precache-manifest.3ec877295b415fc532824adb2d5dc238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d3685ca45a1537e721c9994fe3fca3",
+    "url": "/TPFinaleWeb/index.html"
+  },
+  {
+    "revision": "1ff465ac8c8e3b48ce21",
+    "url": "/TPFinaleWeb/static/css/main.0b9c011b.chunk.css"
+  },
+  {
+    "revision": "eb3c9d1c8b14f53096b4",
+    "url": "/TPFinaleWeb/static/js/2.2a193634.chunk.js"
+  },
+  {
+    "revision": "1ff465ac8c8e3b48ce21",
+    "url": "/TPFinaleWeb/static/js/main.f29affe8.chunk.js"
+  },
+  {
+    "revision": "377b5e5d62ff2825e6d4",
+    "url": "/TPFinaleWeb/static/js/runtime-main.7ad53927.js"
+  }
+]);
